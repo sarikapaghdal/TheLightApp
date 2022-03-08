97da694d15ca391f9324350d78d1746d9a3a7362
Dhaval Shekhada <sarika.paghdal1@gmail.com>
2022-03-08 10:46:00 +0100

    Initial Commit

---
+++ TheLightApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheLightApp
+//
+//  Created by Sarika on 08.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheLightApp/TheLightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLightAppApp.swift
+//  TheLightApp
+//
+//  Created by Sarika on 08.03.22.
+//
+
+import SwiftUI
+
+@main
+struct TheLightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
